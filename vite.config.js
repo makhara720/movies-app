5c367c274fbbf1ae150e0fd250b3fde56919a0a9
makhara720 <lmakharashvili8@gmail.com>
2025-04-09 00:15:11 +0400

    build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: false,
+    minify: true,
+  },
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
   },
 })
